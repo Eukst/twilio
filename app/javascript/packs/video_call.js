d05c0091ffb69b9938a4114e59091395a646a842
Digesh-Parecha <digeshparecha2819@gmail.com>
2021-12-07 19:58:21 +0530

    Added mute/unmute audio/video and share screen feature

--- app/javascript/packs/video_call.js
+++ app/javascript/packs/video_call.js
@@ -46,8 +46,27 @@ window.joinRoom = async function(room, token){
   room.on("participantConnected", onParticipantConnected);
   room.on("participantDisconnected", onParticipantDisconnected);
   $("#call-end-btn").removeClass("d-none");
+  $("#call-mute-btn").removeClass("d-none");
+  $("#video-disable-btn").removeClass("d-none");
+  $("#screen-share-btn").removeClass("d-none");
+  
   $("#call-end-btn").on("click",function() {
     onLeaveButtonClick(room);
+  })
+  $("#call-mute-btn").on("click",function() {
+    onMuteAudioButton(room);
+  })
+  $("#call-unmute-btn").on("click",function() {
+    onUnMuteAudioButton(room);
+  })
+  $("#video-disable-btn").on("click",function() {
+    onDisableVideoButton(room);
+  })
+  $("#video-enable-btn").on("click",function() {
+    onEnableVideoButton(room);
+  })
+  $("#screen-share-btn").on("click",function() {
+    onScreenShareButton(room);
   })
 };
 
@@ -61,7 +80,14 @@ window.onParticipantConnected = function(participant){
     participantDiv.appendChild(track.attach());
   };
   participant.on("trackSubscribed", trackSubscribed);
+  participant.on("trackPublished", (publication) =>{
+    $("#remote-video video:first").addClass("d-none");
+  } );
 
+  participant.on("trackUnpublished", (publication) =>{
+    $("#remote-video video:first").removeClass("d-none");
+  } );
+  
   participant.tracks.forEach((publication) => {
     if (publication.isSubscribed) {
     trackSubscribed(publication.track);
@@ -73,7 +99,16 @@ window.onParticipantConnected = function(participant){
   const trackUnsubscribed = (track) => {
     track.detach().forEach((element) => element.remove());
   };
-
+  participant.on('trackDisabled', track => {
+    if (track.kind == "video"){
+      $("#remote-video video:first").addClass("d-none");
+    }
+  });
+  participant.on('trackEnabled', track => {
+    if (track.kind == "video"){
+      $("#remote-video video:first").removeClass("d-none");
+    }
+  });    
   participant.on("trackUnsubscribed", trackUnsubscribed);
 };
 
@@ -95,3 +130,81 @@ window.onLeaveButtonClick = function(room){
   room.disconnect();
   window.location = '/';
 };
+
+window.onMuteAudioButton = function(room){
+  room.localParticipant.audioTracks.forEach(function(audioTrack) {
+    audioTrack.track.disable();
+  });
+  $("#call-mute-btn").addClass("d-none");
+  $("#call-unmute-btn").removeClass("d-none");
+};
+
+window.onUnMuteAudioButton = function(room){
+  room.localParticipant.audioTracks.forEach(function(audioTrack) {
+    audioTrack.track.enable();
+  });
+  $("#call-mute-btn").removeClass("d-none");
+  $("#call-unmute-btn").addClass("d-none");
+};
+
+window.onDisableVideoButton = function(room){
+  room.localParticipant.videoTracks.forEach(function(videoTrack) {
+    videoTrack.track.disable();
+  });
+  $("#video-disable-btn").addClass("d-none");
+  $("#video-enable-btn").removeClass("d-none");
+};
+
+window.onEnableVideoButton = function(room){
+  room.localParticipant.videoTracks.forEach(function(videoTrack) {
+    videoTrack.track.enable();
+  });
+  $("#video-disable-btn").removeClass("d-none");
+  $("#video-enable-btn").addClass("d-none");
+};
+
+window.createScreenTrack= function(height, width) {
+  const Video = Twilio.Video;
+  if (typeof navigator === 'undefined'
+    || !navigator.mediaDevices
+    || !navigator.mediaDevices.getDisplayMedia) {
+    return Promise.reject(new Error('getDisplayMedia is not supported'));
+  }
+  return navigator.mediaDevices.getDisplayMedia({
+    video: {
+      height: height,
+      width: width
+    }
+  }).then(function(stream) {
+    return new Video.LocalVideoTrack(stream.getVideoTracks()[0]);
+  });
+}
+
+window.PublishedTracks =function( room, track){
+  room.localParticipant.publishTrack(track);
+  let localMediaContainer = document.getElementById("local-video");
+  localMediaContainer.appendChild(track.attach());
+  $("#stop-screen-share-btn").on("click",function() {
+    onStopScreenShareButton(room,track);
+  })
+}
+
+window.StopTracks =function( room, track){
+  room.localParticipant.unpublishTrack(track);
+  track.stop();
+  track = null;
+  let localMediaContainer = document.getElementById("local-video");
+  localMediaContainer.removeChild(localMediaContainer.lastChild);
+}
+
+window.onScreenShareButton = async function(room){
+  let screenTrack = await createScreenTrack(700, 700);
+  PublishedTracks( room, screenTrack );
+  $("#screen-share-btn").addClass("d-none");
+  $("#stop-screen-share-btn").removeClass("d-none");
+}
+window.onStopScreenShareButton = function(room,track){
+  StopTracks( room, track );
+  $("#screen-share-btn").removeClass("d-none");
+  $("#stop-screen-share-btn").addClass("d-none");
+}
